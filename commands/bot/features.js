653e9247d842384d3db589106d03b50f78ae1668
oxids <zerex@outlook.de>
2026-06-17 09:38:21 +0200

    Add raid pool to features list

--- commands/bot/features.js
+++ commands/bot/features.js
@@ -20,12 +20,12 @@ module.exports = {
 		}
 
 		commands = [
-			'**/world-events-tracker**\nAutomated notification and party system for the Annihilation world event.',
 			'**/guild-applications**\nAutomated guild application system which lets users apply for Wynncraft guilds using custom Discord modals.',
 			'**/guild-member-tracker**\nAutomated member tracker for Wynncraft guilds which tracks joins, promotions and leaves.',
 			'**/guild-verification**\nAutomated verification system, which automates giving guild roles to verified users based on their Wynncraft guild ranks.',
+			'**/raid-pool**\nDisplays the current raid pool and can automatically post updates when it changes.',
 			'**/war-tracker**\nAutomated territory tracker for Wynncraft guilds.',
-			'**/changelog**\nDisplays the changelog for the bot.',
+			'**/world-events-tracker**\nAutomated notification and party system for the Annihilation world event.',
 		];
 
 		embeds = _.concat(embeds, DiscordHelper.getEmbeds([{ name: '', value: commands.sort().join('\n\n') }],

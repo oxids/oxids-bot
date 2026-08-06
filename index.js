911736836851dde8d25610aea7e26810da038b18
oxids <zerex@outlook.de>
2026-08-06 07:52:39 +0200

    Fix discordJs retrying API calls, causing random duplications

--- index.js
+++ index.js
@@ -58,11 +58,18 @@ LogHelper.writeToLog('\n\n\n');
 LogHelper.writeToLog('index: Starting...');
 
 try {
-    const client = new Client({ intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMembers
-    ] });
+    const client = new Client({
+        rest: {
+            // Disables automatic retries on timeouts/errors
+            // Needed because Discord API is bad and this causes messages to be sent multiple times
+            retries: 0
+        },
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.GuildMembers
+        ]
+    });
     setCommands(client);
     setEvents(client);
 
